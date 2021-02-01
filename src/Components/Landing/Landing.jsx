90981deb760af52db37965e22db47ee0d2cec490
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2021-02-01 17:51:37 +0530

    hi tag

--- src/Components/Landing/Landing.jsx
+++ src/Components/Landing/Landing.jsx
@@ -54,6 +54,7 @@ export const Landing = () => {
               <Col>
                 <Form.Control type="email" placeholder="No Of Beds" />
               </Col>
+              <h1>hiiii</h1>
             </Row>
           </Form>
           <Button
